48ef63f0f2079e5b4602ccf9c26fcab56e053476
Niwandi <niwandihewagama@gmail.com>
2023-11-19 18:47:58 +0530

    Privacy Policy

--- Pessenger_dashboard/profile-page.php
+++ Pessenger_dashboard/profile-page.php
@@ -17,6 +17,7 @@
         <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+Sinhala:wght@100;300;400;500;600;700;800;900&family=Poppins:wght@400;500;600;700&family=Roboto:wght@100;300;400;500;700;900&family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,900&display=swap"
             rel="stylesheet">
+           
 
         <title>Profile</title>
     </head>
