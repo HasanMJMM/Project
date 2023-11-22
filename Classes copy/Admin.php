94ade2d9a81dbefea4e509141e8945dbbf644a25
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2023-11-22 23:18:08 +0530

    add emp

--- Classes copy/Admin.php
+++ Classes copy/Admin.php
@@ -75,44 +75,45 @@ class Admin
         return ($count > 0);
     }
 
-    public function Emp_Registration($CID, $Name, $Bus_ID)
+    public function Emp_Registration($CID, $ID, $Name)
     {
         $dbcon = new DbConnector();
         $con = $dbcon->getConnection();
-    
-        $query1 = "INSERT INTO conductor(Conductor_ID, Name, Bus_ID) VALUES (?, ?, ?)";
-    
+
+        $query1 = "INSERT INTO conductor(Conductor_ID, Bus_ID, Name) VALUES (?, ?, ?)";
+
         $pstmt1 = $con->prepare($query1);
         $pstmt1->bindValue(1, $CID);
-        $pstmt1->bindValue(2, $Name);
-        $pstmt1->bindValue(3, $Bus_ID);
-    
-        try{
+        $pstmt1->bindValue(2, $ID);
+        $pstmt1->bindValue(3, $Name);
 
-        if ($pstmt1->execute()) {
-            return true;
-        } else {
-            return false;
+        try {
+            if ($pstmt1->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\PDOException $EX) {
+            echo $EX->getMessage();
         }
-    }catch(\PDOException $EX){
-        echo $EX->getMessage(       );
-
     }
+
+    public function EmpExists($CID, $ID)
+{
+    $dbcon = new DbConnector();
+    $con = $dbcon->getConnection();
+
+    $query = "SELECT COUNT(*) FROM conductor WHERE Conductor_ID = ? AND Bus_ID = ?";
+    $pstmt = $con->prepare($query);
+    $pstmt->bindValue(1, $CID);
+    $pstmt->bindValue(2, $ID);
+    $pstmt->execute();
+
+    $count = $pstmt->fetchColumn();
+
+    return ($count > 0);
+}
+
     
-    }
-    public function EmpExists($CID)
-    {
-        $dbcon = new DbConnector();
-        $con = $dbcon->getConnection();
-
-        $query = "SELECT COUNT(*) FROM conductor WHERE Conductor_ID = ?";
-        $pstmt = $con->prepare($query);
-        $pstmt->bindValue(1, $CID);
-        $pstmt->execute();
-
-        $count = $pstmt->fetchColumn();
-
-        return ($count > 0);
-    }
 }
 ?>
